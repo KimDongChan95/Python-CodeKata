c638d5950bf66a3bbe5631426a5c8b176ff32951
KimDongChna95 <impact7608@gmail.com>
2024-10-23 20:58:57 +0900

    0

---
+++ 013.Digit_Sum.py
@@ -0,0 +1,14 @@
+def solution(n):
+        answer = 0
+        for x in list(str(n)):
+            answer += int(x)
+        
+        return answer
+
+print(solution(4583))
+
+
+def solution(N):
+    return sum(int(x) for x in str(N))
+
+print(solution(1263))  # 출력: 6
