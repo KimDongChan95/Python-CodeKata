9b839aafa2d5319dfe0579aa3ce112d8176bf704
KimDongChna95 <impact7608@gmail.com>
2024-11-01 23:12:45 +0900

    review19

--- 019.Integer_Square_Root.py
+++ 019.Integer_Square_Root.py
@@ -1,7 +1,6 @@
-def solution(n):
-    for x in range(1, n + 1):
+def solution(n): #solution함수 정의 
+   for x in range(1, n + 1):
         if x * x == n:
             return (x + 1) * (x + 1)
         elif x * x > n:
-            break
-    return -1
+            return -1 
