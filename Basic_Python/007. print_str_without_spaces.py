95d0eb02859cb8a60337ddde511b8960fab020c7
KimDongChna95 <impact7608@gmail.com>
2024-11-09 23:34:28 +0900

    ss

--- Basic_Python/007. print_str_without_spaces.py
+++ Basic_Python/007. print_str_without_spaces.py
@@ -4,3 +4,9 @@ print(str2)
 
 str1, str2 = input().strip().split(' ')
 print(str1+str2)
+
+#'구분자'. join(리스트) : 리스트에 있는 요소 하나하나를 합쳐서 
+# 하나의 문자열로 바꿔서 변환하고 합칠때, 구분자를 중간중간 넣어줍니다.
+
+result = ''.join([str1, str2])
+print(result)
--- Basic_Python/009.odd_even_check.py
+++ Basic_Python/009.odd_even_check.py
@@ -3,3 +3,5 @@ if a%2==1:
     print(f"{a} is odd")
 else:
     print(f"{a} is even")
+
+    ..gafdsfds
