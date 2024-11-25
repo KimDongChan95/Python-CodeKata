e02d59ec6652ca2b10b8970b90ff0a6e9bdcdc9b
KimDongChna95 <impact7608@gmail.com>
2024-11-25 22:04:28 +0900

    dn

--- Basic_Python/021. Handle_code.py
+++ Basic_Python/021. Handle_code.py
@@ -3,7 +3,7 @@ def solution(code):
     mode = 0  # 시작 모드는 0
 
     for idx in range(len(code)):
-        if code[idx] == "1":  # "1"이 나타나면 모드 전환하기!!
+        if code[idx] == "1":  # "1"이 나타나면 모드 전환하기!!!!
             mode = 1 - mode
         else:
             if (mode == 0 and idx % 2 == 0) or (mode == 1 and idx % 2 != 0):
